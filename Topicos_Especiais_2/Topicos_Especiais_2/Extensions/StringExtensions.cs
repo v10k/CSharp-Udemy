a92efbd6217614abfc91a470d938980a9dc1c8e3
Vinicius Kaiser <vinicius.kaiser43@gmail.com>
2020-05-08 13:53:21 -0300

    Adicionado módulo de tópicos especiais 2

---
+++ Topicos_Especiais_2/Topicos_Especiais_2/Extensions/StringExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace System {
+   static class StringExtensions {
+
+        public static string Cut(this string thisObj, int n) {
+            return (thisObj.Length <= n) ? thisObj : thisObj.Substring(0, n) + "...";
+        }
+    }
+}
---
+++ Topicos_Especiais_2/Topicos_Especiais_2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Topicos_Especiais_2 {
+    class Program {
+        static void Main(string[] args) {
+            DateTime dt = new DateTime(2020, 5, 8, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+
+            string str = "Good Morning";
+            Console.WriteLine(str.Cut(10));
+
+        }
+    }
+}
