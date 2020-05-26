3f84ec91244428abde7d4b99dfb8d07f82f9156b
Vinicius Kaiser <vinicius.kaiser43@gmail.com>
2020-05-26 14:17:23 -0300

    SellerService and basic FindAll

--- netcore/SalesWebMvc/SalesWebMvc/Controllers/SellersController.cs
+++ netcore/SalesWebMvc/SalesWebMvc/Controllers/SellersController.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Services;
 
-namespace SalesWebMvc.Controllers
-{
-    public class SellersController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
+namespace SalesWebMvc.Controllers {
+    public class SellersController : Controller {
+
+        private readonly SellerService _sellerService;
+
+        public SellersController(SellerService sellerService) {
+            _sellerService = sellerService;
+        }
+
+        public IActionResult Index() {
+            var list = _sellerService.FindAll();
+            return View(list);
         }
     }
 }
--- netcore/SalesWebMvc/SalesWebMvc/Startup.cs
+++ netcore/SalesWebMvc/SalesWebMvc/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
+using SalesWebMvc.Services;
 
 namespace SalesWebMvc {
     public class Startup {
@@ -37,6 +38,9 @@ namespace SalesWebMvc {
 
             // Register seeding system
             services.AddScoped<SeedingService>();
+
+            // Register SellerServices system
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
