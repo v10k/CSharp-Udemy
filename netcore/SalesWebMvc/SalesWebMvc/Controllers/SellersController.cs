f934b273c686153147aeb7a16b56026fecf6b477
Vinicius Kaiser <vinicius.kaiser43@gmail.com>
2020-06-09 11:19:36 -0300

    Simple create form

--- netcore/SalesWebMvc/SalesWebMvc/Controllers/SellersController.cs
+++ netcore/SalesWebMvc/SalesWebMvc/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers {
@@ -17,6 +18,18 @@ namespace SalesWebMvc.Controllers {
         public IActionResult Index() {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create() {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller) {
+            _sellerService.Insert(seller);
+            // return RedirectToAction("Index");
+            return RedirectToAction(nameof(Index));
         }
     }
 }
