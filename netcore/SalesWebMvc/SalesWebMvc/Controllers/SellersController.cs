e748b039bb409253282b157e26c8e84cacad5291
Vinicius Kaiser <vinicius.kaiser43@gmail.com>
2020-06-10 13:20:01 -0300

    Delete Seller

--- netcore/SalesWebMvc/SalesWebMvc/Controllers/SellersController.cs
+++ netcore/SalesWebMvc/SalesWebMvc/Controllers/SellersController.cs
@@ -36,5 +36,25 @@ namespace SalesWebMvc.Controllers {
             // return RedirectToAction("Index");
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Delete(int? id) {
+            if (id == null) {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindlById(id.Value);
+            if (obj == null) {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id) {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
