72135c27fc0e09802d0b6716d0c949ef589e5f95
Vinicius Kaiser <vinicius.kaiser43@gmail.com>
2020-06-10 13:42:10 -0300

    Seller details and eager loading

--- netcore/SalesWebMvc/SalesWebMvc/Services/SellerService.cs
+++ netcore/SalesWebMvc/SalesWebMvc/Services/SellerService.cs
@@ -1,4 +1,5 @@
-﻿using SalesWebMvc.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Data;
 using SalesWebMvc.Models;
 using System;
 using System.Collections.Generic;
@@ -24,7 +25,7 @@ namespace SalesWebMvc.Services {
         }
 
         public Seller FindlById(int id) {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id) {
