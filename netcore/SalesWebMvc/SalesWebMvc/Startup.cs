a900efc6876cb97145013a0728a6bd0e7c47b6e5
Vinicius Kaiser <vinicius.kaiser43@gmail.com>
2020-06-13 13:19:27 -0300

    Implementing simple search

--- netcore/SalesWebMvc/SalesWebMvc/Startup.cs
+++ netcore/SalesWebMvc/SalesWebMvc/Startup.cs
@@ -46,6 +46,9 @@ namespace SalesWebMvc {
 
             // Register DepartmentService
             services.AddScoped<DepartmentService>();
+
+            // Register SalesRecordService
+            services.AddScoped<SalesRecordService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
